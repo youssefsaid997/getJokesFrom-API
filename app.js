12fd3a12ba36e026f5740fc1cfaea74fd846d77c
youssefsaid997 <57658169+youssefsaid997@users.noreply.github.com>
2023-02-15 03:25:19 +0200

    we added everthing for one joke but we will extend more with foreach

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-document.querySelector('form').addEventListener('click',function(e){
+document.querySelector('#btn').addEventListener('click',function(e){
     const number = document.getElementById('number').value;
     console.log(number);
 
+    const xhr = new XMLHttpRequest()
 
+    xhr.open('GET',`https://api.chucknorris.io/jokes/random/${number}`,true);
 
+    let output= '';
 
+    xhr.onload=function(){
+        if(this.status===200){
+            const resText= JSON.parse(this.responseText);
+            console.log(resText.value)
+            
+                output+=`
+                <li>${resText.value}</li>
+                `;
+            }else{
+                output+=`<li>Something went Wrong</li>`
+            
+        }
+        document.getElementById('jokes').innerHTML=output;
+    }
+
+    xhr.send();
     e.preventDefault();
 })
