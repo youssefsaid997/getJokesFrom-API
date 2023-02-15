0f01434fd4e4ba457c409e91058b48e544fa4175
youssefsaid997 <57658169+youssefsaid997@users.noreply.github.com>
2023-02-15 02:55:40 +0200

    adding the script

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector('form').addEventListener('click',function(e){
+    const number = document.getElementById('number').value;
+    console.log(number);
+
+
+
+
+    e.preventDefault();
+})
